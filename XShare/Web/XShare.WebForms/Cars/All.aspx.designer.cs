641323d24fc7c1d9891b9a7dd13c1bc079289537
Chavdar Angelov <c.l.angelov@gmail.com>
2016-01-22 20:40:50 +0200

    pagintion

--- XShare/Web/XShare.WebForms/Cars/All.aspx.designer.cs
+++ XShare/Web/XShare.WebForms/Cars/All.aspx.designer.cs
@@ -20,5 +20,14 @@ namespace XShare.WebForms.Cars {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.ListView ViewAllCars;
+        
+        /// <summary>
+        /// DataPagerAll control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DataPager DataPagerAll;
     }
 }
