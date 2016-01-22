c42d322c85c6a864e12e32f0ccf9dcbd85c9b794
Ivo Paounov <ivo.paunov@gmail.com>
2016-01-22 14:25:47 +0200

    Custom Errors. 404 Not found custom error response.

--- XShare/Web/XShare.WebForms/Global.asax.cs
+++ XShare/Web/XShare.WebForms/Global.asax.cs
@@ -1,17 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Optimization;
-using System.Web.Routing;
-using System.Web.Security;
-using System.Web.SessionState;
-
-namespace XShare.WebForms
+﻿namespace XShare.WebForms
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+    using System.Web.Optimization;
+    using System.Web.Routing;
+    using System.Web.Security;
+    using System.Web.SessionState;
+
     public class Global : HttpApplication
     {
-        void Application_Start(object sender, EventArgs e)
+        private void Application_Start(object sender, EventArgs e)
         {
             // Code that runs on application startup
             RouteConfig.RegisterRoutes(RouteTable.Routes);
