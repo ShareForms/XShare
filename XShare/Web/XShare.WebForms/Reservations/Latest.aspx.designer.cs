1a22e70c320336ac8f2a56f522f509189e53bfcc
Ivo Paounov <ivo.paunov@gmail.com>
2016-01-23 01:02:26 +0200

    Reservations added pagination and sorting.

--- XShare/Web/XShare.WebForms/Reservations/Latest.aspx.designer.cs
+++ XShare/Web/XShare.WebForms/Reservations/Latest.aspx.designer.cs
@@ -22,12 +22,12 @@ namespace XShare.WebForms.Reservations {
         protected global::System.Web.UI.WebControls.ListView ViewLastestReservations;
         
         /// <summary>
-        /// test control.
+        /// DataPagerAllReservations control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Literal test;
+        protected global::System.Web.UI.WebControls.DataPager DataPagerAllReservations;
     }
 }
