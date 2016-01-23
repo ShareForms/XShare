92d7bc82ed4e8bab15c581323a6a5bd9d12d1b22
Chavdar Angelov <c.l.angelov@gmail.com>
2016-01-23 15:21:05 +0200

    rating ready and kikin'

--- XShare/Web/XShare.WebForms/Cars/CarDetails.aspx.cs
+++ XShare/Web/XShare.WebForms/Cars/CarDetails.aspx.cs
@@ -31,5 +31,20 @@
             var carToDisplay = this.CarService.CarById((int)carID);
             return carToDisplay;
         }
+
+        public IQueryable<int> GetCarRatings()
+        {
+            return (new[] { 1, 2, 3, 4, 5 }).AsQueryable();
+        }
+
+        protected void Btn_RateCar(object sender, EventArgs e)
+        {
+            var carRating = int.Parse(this.CarRate.SelectedValue);
+            var carId = int.Parse(this.Request.QueryString["id"]);
+
+            this.CarService.AddRating(carId, carRating);
+
+            this.Response.Redirect(Request.RawUrl);
+        }
     }
 }
